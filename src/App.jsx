c788592f09f68e1353c0e76ffafaa5ebecbb448f
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-12-03 03:25:20 +0545

    avatar and urls added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <UserItem />
     </div>
   );
 }
